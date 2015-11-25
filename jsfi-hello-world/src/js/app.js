3e81718f9f4855c40aa940395668bbfc3521fa79
Martin Sachse <martin.sachse+jsfi@gmail.com>
2015-11-25 23:51:37 +0100

    added hello world

---
+++ jsfi-hello-world/src/js/app.js
@@ -0,0 +1,8 @@
+var UI = require('ui');
+
+var card = new UI.Card({
+    title: 'Hello World',
+    subtitle: 'My first Pebble App'
+});
+
+card.show();
---
+++ jsfi-hello-world/src/js/main.js
@@ -0,0 +1,4 @@
+Pebble.addEventListener('ready', function() {
+    require('ui/simply-pebble.js').init();
+    require('app.js');
+});
